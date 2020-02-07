9556b9413d1e83459f4cd0f51a65478300617e65
bryan2000729 <60495768+bryan2000729@users.noreply.github.com>
2020-02-06 21:28:19 -0600

    Add files via upload

---
+++ P2.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+	int a=6;
+	int b=2;
+	a+b;
+	a-b;
+	a*b;
+	a/b;
+	printf("%d+%d=%d\n",a,b,a+b);
+	printf("%d-%d=%d\n",a,b,a-b);
+	printf("%d*%d=%d\n",a,b,a*b);
+	printf("%d/%d=%d\n",a,b,a/b);
+	return 0;
+}

4cab9a2b2f050c94157f1ac50bd24f1cea687b55
bryan2000729 <60495768+bryan2000729@users.noreply.github.com>
2020-03-13 19:37:36 -0600

    Update P8.cpp

--- P8.cpp
+++ P8.cpp
@@ -1,13 +1,19 @@
 #include<stdio.h>
 int main(void){
-	int x, i=1, n, p, y;
+	int x, i=1, n, y=1, c==1, d==2;
 	printf("Introduce x: ");
 	scanf("%d",&x);
 	if(x>0&&x<100){
 	for(i=1;i<=x;i++){
 		n=i;
-		printf("%d\n",n);
 	}
+		for(y=1;y<=x;y++){
+			if(n/x=c1){
+				printf("%d es numero primo", n);
+			}else{
+				printf("%d no es numero primo", n);
+			}
+		}
 }else{
 	printf("Error");
 }
